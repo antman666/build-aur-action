6e0141253925272cb2d776e00e11172f150aa211
antman666 <945360554@qq.com>
2022-03-12 20:04:18 +0800

    fix glibc

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # export PACKAGER="AntMan666 <945360554@qq.com>"
 
-git clone "https://aur.archlinux.org/$1.git"
+git clone "https://github.com/antman666/$1.git"
 cd "$1"
 echo 'PACKAGER="AntMan666 <945360554@qq.com>"' >> /etc/makepkg.conf
 updpkgsums
